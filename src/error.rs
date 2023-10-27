854a533930ce5c7e625c33a71059b1497f4fbbe5
xxcodianxx <44289933+xxcodianxx@users.noreply.github.com>
2023-10-27 23:49:59 +1030

    feat(docs): add docs for new error handling

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,15 @@ use crate::auth::token::TokenParseError;
 
 pub type HResult<T> = std::result::Result<T, HandlerError>;
 
+/// A custom error type that should be used for all errors that occur in the
+/// route handler functions. This type implements `ResponseError` and can be
+/// returned from a handler function to send an error response to the client.
+///
+/// Conveniently, this type implements `From` for `sqlx::Error` and `u16` to
+/// make it easy to return an error from a database query or a status code.
+///
+/// It is recommended that you use the `err!` macro to return an error from a
+/// handler function. This macro is re-exported from this module.
 #[derive(Debug, Display, Error, Serialize)]
 #[display(fmt = "{}", message)]
 pub struct HandlerError {
@@ -83,7 +92,27 @@ impl From<TokenParseError> for HandlerError {
     }
 }
 
-
+/// Trait to facilitate the conversion between `Result` and `Option` into a
+/// `HandlerError`. Useful when you have a `Result` or `Option` and want to
+/// return an error if it is `None` or `Err`.
+///
+/// Example:
+/// ```
+///
+/// fn handler(number: &str, double: &str) -> HResult<i32> {
+///     //                                    ~~~~~~~~~~~~ use when possible
+///     // Returns HTTP 400 Bad Request
+///     let mut number: i32 = number.parse().or_err(400)?;
+///     // Returns HTTP 400 Bad Request with a custom message
+///     let double: bool = double.parse().or_err_msg(400, "must be true or false")?;
+///
+///     if double {
+///         number *= 2;
+///     }
+///
+///     Ok(number)
+/// }
+/// ```
 pub trait IntoHandlerErrorResult<T> {
     fn or_err(self, code: u16) -> Result<T, HandlerError>;
     fn or_err_msg(self, code: u16, message: &'static str) -> Result<T, HandlerError>;
@@ -109,19 +138,40 @@ impl<T> IntoHandlerErrorResult<T> for Option<T> {
     }
 }
 
-
 pub mod macros {
-macro_rules! err {
-    ($code:expr, $msg:expr) => {
-        Err(crate::error::HandlerError::from(($code, $msg.into())))
-    };
-    ($code:expr) => {
-        Err(crate::error::HandlerError::from($code))
-    };
-    () => {
-        Err(crate::error::HandlerError::internal_error())
-    };
-}
+    /// Convenience macro to return a `HandlerError`` wrapped in an `Err()``.
+    /// Use it whenever you want to return an error from a handler function.
+    /// 
+    /// Example:
+    /// ```
+    /// use crate::error::macros::err;
+    /// 
+    /// fn handler() -> HResult<()> {
+    ///    // Returns HTTP 500 Internal Server Error
+    ///    err!()?;
+    ///    
+    ///    // Returns HTTP 403 Forbidden
+    ///    err!(403)?;
+    ///    
+    ///    // Returns HTTP 403 Forbidden with a custom message
+    ///    err!(403, "You shall not pass!")?;
+    ///    
+    ///    // Returns HTTP 500 Internal Server Error with a custom message
+    ///    err!("A rat chewed through the ethernet cable")?;
+    ///    
+    ///    Ok(())
+    /// }
+    macro_rules! err {
+        ($code:expr, $msg:expr) => {
+            Err(crate::error::HandlerError::from(($code, $msg.into())))
+        };
+        ($code:expr) => {
+            Err(crate::error::HandlerError::from($code))
+        };
+        () => {
+            Err(crate::error::HandlerError::internal_error())
+        };
+    }
 
     pub(crate) use err;
 }
