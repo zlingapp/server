269e47a2d1724f5a8965eb3f3749019d5b07b739
xxori <ori@kek.church>
2023-11-01 00:30:42 +1030

    chore: Simple integration testing -TODO Add more test coverage, e.g. guild creation, messaging, websocket stuff (?), friend requests

---
+++ tests/test_auth.rs
@@ -0,0 +1,40 @@
+use actix_web::{http::header::ContentType, test, web::Data, App};
+use serde::Serialize;
+use sqlx::{query, Pool, Postgres};
+use zling_server::{auth, db::Database};
+
+#[derive(Serialize)]
+struct RegisterPayload {
+    email: String,
+    password: String,
+    username: String,
+}
+
+#[sqlx::test]
+async fn test_successful_registration(pool: Pool<Postgres>) {
+    let db = Data::new(Database::with_pool(pool.clone()));
+    let app = test::init_service(
+        App::new()
+            .app_data(db)
+            .configure(auth::routes::configure_app),
+    )
+    .await;
+
+    let req = test::TestRequest::post()
+        .uri("/auth/register")
+        .set_json(RegisterPayload {
+            email: "test@example.com".into(),
+            password: "password123".into(),
+            username: "Test User".into(),
+        })
+        .insert_header(ContentType::json())
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
+    let user = query!(r#"SELECT * FROM users"#)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    assert_eq!(user.email, Some("test@example.com".into()));
+    assert_eq!(user.name.split("#").collect::<Vec<&str>>()[0], "Test User");
+}
