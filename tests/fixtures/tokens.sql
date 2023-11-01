1c3942b7ecc1dbccd9208276d2949bf0c71391b1
xxori <ori@kek.church>
2023-11-01 15:48:09 +1030

    feat: Token fixtures in tests +Allows us to skip login and registration and reissuing for privileged tests i.e. messages, channels, guilds

---
+++ tests/fixtures/tokens.sql
@@ -0,0 +1,14 @@
+-- Access Token oUR5y3ph_EFASnt037BCK.ZUUnGQ.J16TLUl6Zg07tj413e2zJU8Qe3A7P6Aiq50qwl-t42o
+-- Refresh token oUR5y3ph_EFASnt037BCK.eA3SoQ.G0K3Li9oAgS7qr8DWXYW6Bhz2IPtXx1r6Z15CowvRtFyT55a
+-- Both valid for 10 years (10/2033)
+-- Make sure to set TOKEN_SIGNING_KEY to 3c6099c189b60fc2843d7059d33b1de1bc05d3b5b6d74bf249db4d8dcd844e62
+INSERT INTO
+    tokens (token_id, nonce, user_id, expires_at, user_agent)
+VALUES
+    (
+        'svIJeTw1PDySyqoeMtXiB',
+        'G0K3Li9oAgS7qr8DWXYW6Bhz2IPtXx1r6Z15CowvRtFyT55a',
+        'oUR5y3ph_EFASnt037BCK',
+        to_timestamp (2014171809),
+        'test'
+    );
--- tests/test_auth.rs
+++ tests/test_auth.rs
@@ -1,10 +1,16 @@
-use actix_web::{test, web::Data, App};
+use actix_web::{dev::ServiceResponse, test, web::Data, App};
+use serde::de::DeserializeOwned;
 use serde_json::json;
 use sqlx::{query, Pool, Postgres};
-use zling_server::{auth, db::Database};
+use std::env::set_var;
+use zling_server::{
+    auth,
+    auth::{routes::login::LoginResponse, user::User},
+    db::Database,
+};
 
 #[sqlx::test]
-async fn test_successful_registration(pool: Pool<Postgres>) {
+async fn test_registration(pool: Pool<Postgres>) {
     let db = Data::new(Database::with_pool(pool.clone()));
     let app = test::init_service(
         App::new()
@@ -23,20 +29,28 @@ async fn test_successful_registration(pool: Pool<Postgres>) {
         .to_request();
 
     let resp = test::call_service(&app, req).await;
-    assert!(resp.status().is_success());
+    assert!(
+        resp.status().is_success(),
+        "registration status code {}",
+        resp.status().as_str()
+    );
 
     let user = query!(r#"SELECT * FROM users"#)
         .fetch_one(&pool)
         .await
         .unwrap();
 
-    assert_eq!(user.email, Some("test@example.com".into()));
+    assert_eq!(
+        user.email,
+        Some("test@example.com".into()),
+        "testing whether the database contains the correct user"
+    );
     assert_eq!(user.name.split("#").next(), Some("Test User"));
 }
 
 #[sqlx::test(fixtures("users"))]
-async fn test_login_whoami(pool: Pool<Postgres>) {
-    let db = Data::new(Database::with_pool(pool.clone()));
+async fn test_login(pool: Pool<Postgres>) {
+    let db = Data::new(Database::with_pool(pool));
     let app = test::init_service(
         App::new()
             .app_data(db)
@@ -51,8 +65,12 @@ async fn test_login_whoami(pool: Pool<Postgres>) {
             "password": "password123",
         }))
         .to_request();
-    let resp: auth::routes::login::LoginResponse = test::call_and_read_body_json(&app, req).await;
-    assert_eq!(resp.user.name, "James#1119");
+    let resp = test::call_service(&app, req).await;
+    let resp: LoginResponse = resp_deserialize(resp).await;
+    assert_eq!(
+        resp.user.name, "James#1119",
+        "testing response from initial login"
+    );
 
     let req = test::TestRequest::get()
         .uri("/auth/whoami")
@@ -61,9 +79,63 @@ async fn test_login_whoami(pool: Pool<Postgres>) {
             "Bearer ".to_owned() + &resp.access_token.to_string(),
         ))
         .to_request();
-    let resp: auth::user::User = test::call_and_read_body_json(&app, req).await;
+    let resp = test::call_service(&app, req).await;
+    let resp: User = resp_deserialize(resp).await;
+    assert_eq!(
+        resp.name, "James#1119",
+        "testing that whoami returns the correct user"
+    );
+    assert_eq!(resp.email, Some("jthomas@example.com".into()));
+    assert_eq!(resp.bot, false);
+    assert_eq!(resp.id, "oUR5y3ph_EFASnt037BCK");
+}
+
+#[sqlx::test(fixtures("users", "tokens"))]
+async fn test_whoami(pool: Pool<Postgres>) {
+    // TODO find some way to set this globally in integration tests
+    // if cfg!(test) only works for unit tests, not calls from lib
+    set_var(
+        "TOKEN_SIGNING_KEY",
+        "3c6099c189b60fc2843d7059d33b1de1bc05d3b5b6d74bf249db4d8dcd844e62",
+    );
+    let db = Data::new(Database::with_pool(pool));
+    let app = test::init_service(
+        App::new()
+            .app_data(db.clone())
+            .configure(auth::routes::configure_app),
+    )
+    .await;
+    let req = privileged_get("/auth/whoami").to_request();
+    let resp = test::call_service(&app, req).await;
+    let resp: User = resp_deserialize(resp).await;
     assert_eq!(resp.name, "James#1119");
     assert_eq!(resp.email, Some("jthomas@example.com".into()));
     assert_eq!(resp.bot, false);
     assert_eq!(resp.id, "oUR5y3ph_EFASnt037BCK");
+}
+
+/// actix_web will just throw a deserialization error if you dont get a 200 code.
+/// This makes it a bit more readable.
+async fn resp_deserialize<T>(resp: ServiceResponse) -> T
+where
+    T: DeserializeOwned,
+{
+    assert!(
+        resp.status().is_success(),
+        "Call to {} failed with status code {}",
+        resp.request().uri(),
+        resp.status().as_str()
+    );
+    test::read_body_json(resp).await
+}
+
+// Attach necessary headers to make a privileged call (using fixed token and user from /fixtures)
+fn privileged_get(endpoint: &str) -> actix_web::test::TestRequest {
+    test::TestRequest::get()
+        .uri(endpoint)
+        .insert_header((
+            "Authorization",
+            "Bearer oUR5y3ph_EFASnt037BCK.ZUUnGQ.J16TLUl6Zg07tj413e2zJU8Qe3A7P6Aiq50qwl-t42o",
+        ))
+        .insert_header(("User-Agent", "test"))
 }
