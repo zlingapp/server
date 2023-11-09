f988f698f95b6bcbd2797eae7b0704bfb834d327
xxori <ori@kek.church>
2023-11-09 18:01:24 +1030

    feat(db): Update schema

---
+++ migrations/20231109072809_dmchannel.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dmchannels (
+    id          text            NOT NULL PRIMARY KEY,
+    from_user   text            NOT NULL,
+    to_user     text            NOT NULL,
+    created_at  timestamp       NOT NULL DEFAULT now(),
+    updated_at  timestamp       NOT NULL DEFAULT now(),
+    permissions json            NOT NULL DEFAULT '{}'::json
+);
